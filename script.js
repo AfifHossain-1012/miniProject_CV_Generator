cbac4f4d4f4fc40fd2131a2504e91792e4470ab5
Afif <afifhossain1012@gmail.com>
2023-09-27 21:13:21 +0600

    full CV generator done

--- script.js
+++ script.js
@@ -27,3 +27,64 @@ function addAQField()
 
     aqObj.insertBefore(newNode, aqAddButtonObj);
 }
+function generateCV(){
+    // console.log("GeneratingCV");
+    let nameField = document.getElementById('nameField').value;
+
+    document.getElementById("nameT1").innerHTML = nameField;
+    document.getElementById("nameT2").innerHTML = nameField;
+
+    let contactField = document.getElementById('contactField').value;
+    document.getElementById("contactT").innerHTML = contactField;
+
+    let addressField = document.getElementById('addressField').value;
+    document.getElementById("addressT").innerHTML = addressField;
+
+    let fbField = document.getElementById('fbField').value;
+    document.getElementById("fbT").innerHTML = fbField;
+
+    let igField = document.getElementById('igField').value;
+    document.getElementById("igT").innerHTML = igField;
+
+    let liField = document.getElementById('liField').value;
+    document.getElementById("liT").innerHTML = liField;
+
+    document.getElementById("objectiveT").innerHTML = document.getElementById("objectiveField").value;
+
+     
+
+    // WORK EXPERIENCE
+    let wes = document.getElementsByClassName('weField');
+    let str = "";
+
+    for(let e of wes)
+    {
+        str = str + '<li>' + e.value + '</li>';
+    }
+    let weTElement = document.getElementById("weT");
+    if (weTElement) {
+        weTElement.innerHTML = str;
+    }
+
+
+
+    // // ACADEMIC QUALIFICATION
+    let aqs = document.getElementsByClassName('aqField');
+    let str1 = "";
+
+    for (let e of aqs) {
+        str1 = str1 + '<li>' + e.value + '</li>';
+    }
+    let aqTElement = document.getElementById("aqT");
+    if (aqTElement) {
+        aqTElement.innerHTML = str1;
+    }
+
+    document.getElementById('cv-form').style.display = "none";
+    document.getElementById('cv-tamplate').style.display = "block";
+
+}
+
+function printCV(){
+    window.print();
+}
